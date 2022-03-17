b3830abd992af0c06c221f7263f1cdea29f3c7a7
franck berneron <franck@axires.tech>
2022-03-17 20:37:52 +0100

    working model

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import tempfile
 # import pdfplumber
 from pathlib import Path
 import requests
+import os
+
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type='pdf')
 if uploaded_file is not None:
@@ -13,36 +15,16 @@ if uploaded_file is not None:
         f.write((uploaded_file).getbuffer())
         storage_upload(f'{uploaded_file.name}', f'{uploaded_file.name}', rm=True)
 
+    url = f'https://unicorn-jkpbyd77ba-ew.a.run.app/question?pdf_name={uploaded_file.name}'
 
+    response = requests.get(url)
 
-# A supprimer 
-import datetime
+    prediction = response.json()
 
-pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_datetime = f'{pickup_date} {pickup_time}'
-pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+    for question, content in prediction.items():
+        st.markdown (f"""#{question} """)
+#        for content_item in content.items():
+        st.markdown (f"""answer: {content['answer']}""")
+        st.markdown (f"""confidence :{content['score']}""")
 
-
-# enter here the address of your flask api
-url = 'https://taxifare.lewagon.ai/predict'
-
-params = dict(
-    pickup_datetime=pickup_datetime,
-    pickup_longitude=pickup_longitude,
-    pickup_latitude=pickup_latitude,
-    dropoff_longitude=dropoff_longitude,
-    dropoff_latitude=dropoff_latitude,
-    passenger_count=passenger_count)
-
-response = requests.get(url, params=params)
-
-prediction = response.json()
-
-pred = prediction['fare']
-
-pred
+ 
--- uploader.py
+++ uploader.py
@@ -7,17 +7,18 @@ from google.cloud import storage
 
 
 def storage_upload(pdf_file, local_path, rm=False):
+    print(pdf_file)
     client = storage.Client().bucket(BUCKET_NAME)
 
     local_file_path = f'../cm_project_contracts_pdf/{pdf_file}'
     storage_location = f"pdf/{pdf_file}"
     blob = client.blob(storage_location)
-    blob.upload_from_filename(f'{local_file_path}')
-    print(f"=> model.joblib uploaded to bucket {BUCKET_NAME} inside {storage_location}")
+    blob.upload_from_filename(f'{pdf_file}')
+#    blob.upload_from_filename(f'{local_file_path}')
+    print(f"=> {pdf_file} uploaded to bucket {BUCKET_NAME} inside {storage_location}")
     if rm:
-        os.remove('model.joblib')
+        os.remove(f'{pdf_file}')
 
 if __name__ == '__main__' :
     storage_upload('UnionDentalHoldingsInc_20050204_8-KA_EX-10_3345577_EX-10_Affiliate Agreement.pdf', '../cm_project_contracts_pdf/')
-
 
