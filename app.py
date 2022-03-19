00d624d547cda7aaa1e7cfcead0347ce255f0b8c
franck berneron <franck@axires.tech>
2022-03-19 10:25:08 +0100

    Heroku push of working interface

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import requests
 import os
 
+st.title('The Contract Manager')
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type='pdf')
 if uploaded_file is not None:
@@ -22,9 +23,8 @@ if uploaded_file is not None:
     prediction = response.json()
 
     for question, content in prediction.items():
-        st.markdown (f"""#{question} """)
-#        for content_item in content.items():
-        st.markdown (f"""answer: {content['answer']}""")
-        st.markdown (f"""confidence :{content['score']}""")
+        st.markdown (f"""# {question}""")
+        st.markdown(f"""## answer: {content['answer']}""")
+        st.markdown(f"""## confidence :{content['score']}""")
 
  
