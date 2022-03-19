d46c011e6aa217112bf0e13ef74e239e5b14d072
franck berneron <franck@axires.tech>
2022-03-19 13:42:20 +0100

    Updated the front end with Bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from uploader import storage_upload
 from io import StringIO
 import base64
@@ -7,6 +8,9 @@ import tempfile
 from pathlib import Path
 import requests
 import os
+st.markdown('''
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+''', unsafe_allow_html=True)
 
 # create credentials file
 google_credentials_file = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
@@ -29,6 +33,21 @@ else:
 
     print("credentials file already exists 🎉")
 
+def card(question, answer, score):
+    return f'''
+    <div class="card">
+        <div class="card-header">
+            {question}
+        </div>
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <p>{answer}</p>
+                <footer class="blockquote-footer">Confidence score: {score}%</footer>
+            </blockquote>
+        </div>
+    </div>
+    '''
+
 st.title('The Contract Manager')
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type='pdf')
@@ -43,9 +62,11 @@ if uploaded_file is not None:
 
     prediction = response.json()
 
-    for question, content in prediction.items():
-        st.markdown (f"""# {question}""")
-        st.markdown(f"""## answer: {content['answer']}""")
-        st.markdown(f"""## confidence :{content['score']}""")
+    if prediction:
+        for question, content in prediction.items():
+            #return card(question, content['answer'], content['score'])
+            st.markdown(card(question, content['answer'], round(content['score']* 100, 2),), unsafe_allow_html=True)
 
- 
+        #        st.markdown (f"""#{question} """)
+        #        st.markdown(f"""## answer: {content['answer']}""")
+        #        st.markdown(f"""## confidence :{content['score']}""") 
